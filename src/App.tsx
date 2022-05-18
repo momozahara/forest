8d7c35c3f15063d5ddfae785384dabbb6afb58a9
Pream Pinbut <momozahara@hotmail.com>
2022-05-18 21:42:13 +0700

    add transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,40 @@
 import React from 'react';
 import { Nav, Navbar, Button } from 'react-bootstrap';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Contact from './Contact';
-import About from './About';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Router from './Router';
 import './App.css';
 
-export default class App extends React.Component<{}, {}> {
-	constructor(props: {}) {
-		super(props);
-	}
-	
-	render() {
-		return (
-			<div>
-				<Navbar fixed='top' bg='light' expand="lg">
-					<Navbar.Brand href="/">Forest</Navbar.Brand>
-					<Button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</Button>
-					<Navbar.Collapse id='navbarNav'>
-						<Nav as='ul'>
-							<Nav.Item as='li'>
-								<Nav.Link href='/'>Home</Nav.Link>
-							</Nav.Item>
-							<Nav.Item as='li'>
-								<Nav.Link href='/contact'>Contact</Nav.Link>
-							</Nav.Item>
-							<Nav.Item as='li'>
-								<Nav.Link href='/about'>About</Nav.Link>
-							</Nav.Item>
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-				<div className='banner'>
-					<h1 className='banner-header'>Forest</h1>
-				</div>
-				<BrowserRouter>
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/contact' element={<Contact />} />
-						<Route path='/about' element={<About />} />
-					</Routes>
-				</BrowserRouter>
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Navbar fixed='top' bg='light' expand="lg">
+				<Navbar.Brand href="/">Forest</Navbar.Brand>
+				<Button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</Button>
+				<Navbar.Collapse id='navbarNav'>
+					<Nav as='ul'>
+						<Nav.Item as='li'>
+							{/* <Nav.Link href='/'>Home</Nav.Link> */}
+							<Link className='nav-link' to='/'>Home</Link>
+						</Nav.Item>
+						<Nav.Item as='li'>
+							{/* <Nav.Link href='/contact'>Contact</Nav.Link> */}
+							<Link className='nav-link' to='/contact'>Contact</Link>
+						</Nav.Item>
+						<Nav.Item as='li'>
+							{/* <Nav.Link href='/about'>About</Nav.Link> */}
+							<Link className='nav-link' to='/about'>About</Link>
+						</Nav.Item>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			<div className='banner'>
+				<h1 className='banner-header'>Forest</h1>
 			</div>
-		)
-	}
-	
+			<Router  />
+		</BrowserRouter>
+	)
 }
+
+export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+
+const Router = () => {
+	let location = useLocation();
+	return (
+		<TransitionGroup>
+			<CSSTransition
+				key={location.pathname}
+				classNames='fade'
+				timeout={300}
+			>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='/about' element={<About />} />
+				</Routes>
+			</CSSTransition>
+		</TransitionGroup>
+	);
+}
+
+export default Router;
