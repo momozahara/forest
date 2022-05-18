14901b9fabd3da7c007c9487a872e2ee3c803157
Pream Pinbut <momozahara@hotmail.com>
2022-05-19 05:05:10 +0700

    update color theme

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import './App.css';
 export default () => {
   return (
     <BrowserRouter>
-      <Navbar fixed='top' bg='light' expand="lg">
+      <Navbar fixed='top' expand="lg">
         <Link className='navbar-brand' to="/">Forest</Link >
-        <Button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <Button variant='light' className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </Button>
         <Navbar.Collapse id='navbarNav'>
@@ -30,7 +30,7 @@ export default () => {
         </Navbar.Collapse>
       </Navbar>
       <div className='banner'>
-        <h1 className='banner-header'>Forest</h1>
+        <h1 className='banner-header no-select'>Forest</h1>
       </div>
       <Router  />
     </BrowserRouter>
